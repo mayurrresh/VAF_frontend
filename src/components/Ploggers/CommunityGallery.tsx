760861fff39f53fa67c34cd6208e975cb7c86977
mayurrresh <mayureshkahar777@gmail.com>
2026-03-05 18:44:27 +0530

    optimised images and website fixes

--- src/components/Ploggers/CommunityGallery.tsx
+++ src/components/Ploggers/CommunityGallery.tsx
@@ -1,6 +1,6 @@
- import communityImage1 from "@/assets/Ploggers/20230709090250_IMG_0191.jpg";
- import communityImage2 from "@/assets/Ploggers/20250406083235_JF0A0410.jpg";
- import communityImage3 from "@/assets/Ploggers/20230709093122_IMG_0282.jpg";
+ import communityImage1 from "@/assets/Ploggers/20230709093122_IMG_0282.JPG";
+ import communityImage2 from "@/assets/Ploggers/20230430072624_IMG_7134.JPG";
+ import communityImage3 from "@/assets/Ploggers/20230430074828_IMG_7187.JPG";
  
  const CommunityGallery = () => {
    const images = [
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.webp";
+declare module "*.svg";
+declare module "*.JPG";
+declare module "*.JPEG";
+declare module "*.PNG";
+declare module "*.WEBP";
+declare module "*.SVG";
+declare module "*.HEIC";
